64776a8b92b08c20eea5467b8b521d0d8e565401
Adedamola Ajayi <adedamola.ajayi@titantrustbank.com>
2021-10-11 17:31:57 +0100

    first commit

---
+++ Jobs/HelloJob.cs
@@ -0,0 +1,16 @@
+﻿using Quartz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QuartzJobs.Jobs
+{
+    public class HelloJob : IJob
+    {
+        public async Task Execute(IJobExecutionContext context)
+        {
+            await Console.Out.WriteLineAsync("HelloJob is executing.");
+        }
+    }
+}
